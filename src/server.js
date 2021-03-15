0dc031d827480e426e5b97037a6c40f128e1fdce
Diego <diego.nicolas.gandara@gmail.com>
2021-03-15 18:00:10 -0300

    Instalamos el SDK de mercadolibre, lo requerimos y empezamos a configurarlo en MODO PRUEBA. Para ello, haremos pruebas con postman

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3000 || process.env.PORT;
+//Agregamos/instanciamos el SDK (Software Development Kit) mercadopago
+const mercadopago = require('mercadopago');
+
+// SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
+
+// Agrega credenciales
+mercadopago.configure({
+  access_token: 'PROD_ACCESS_TOKEN' //Todavía no vamos a poner el TOKET real: para hacer prueba vamos a usar USUARIOS DE PRUEBA.
+});
+
+        
+
 //Server
 
 app.get('/checkout', (req,res) => {
-    res.send(`<h1>Hola desde checkout </h1>`)
+    res.send(`<h1>Hola desde checkout  </h1>`)
 })
 
 
