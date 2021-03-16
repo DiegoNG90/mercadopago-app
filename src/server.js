3fa3fa70226ec2e46e7ddbb4a71de1769891d56e
Diego <diego.nicolas.gandara@gmail.com>
2021-03-15 22:46:41 -0300

    instancio bodyParser, lo llamo en los middlewares y agrego otros que tienen la misma funcionalidad -dado que bodyParser me aparece como deprecado-. Conforme a la documentacion de la integracion Checkout Pro de mercadopago https://www.mercadopago.com.ar/developers/es/guides/online-payments/checkout-pro/integration, debo configurar la instancia de mercadopago para que acceda con un access token. Como vimos previamente en el commit anterior, yo NO ESTOY TRABAJANDO CON MIS CREDENCIALES REALES, sino que cree dos usuers de TESTEO que estan guardados en el archivo fake_users.json a traves de POSTMAN. Por ende, al objeto de configuracion, le paso el access token del 1er fake_user. Luego de ésto, se la documentacion exige definir una 'preferencia'. Esta la vamos a crear al interior de la ruta post /checkout para que, cuando el user lance el boton de pagar, llegue acá la info. Por ende, como la data que el user acepta comprar esta en el form, llegará acá como REQUEST. Entonces, la 'preference' la vamos a definir con el body de la request. Luego, una vez que mercadopago crea la preferencia con nuestros valores, vamos a hacer un redirect al end-point de inicio de mercadopago (que es response.body.init_point).

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3000 || process.env.PORT;
+const bodyParser = require('body-parser');
 //Agregamos/instanciamos el SDK (Software Development Kit) mercadopago
 const mercadopago = require('mercadopago');
 
+
+
+//Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
 // SDK de Mercado Pago
 
 // Agrega credenciales
@@ -11,12 +18,32 @@ mercadopago.configure({
   access_token: "APP_USR-6308537224528879-031521-a5fcad24749177e4f6604d77f70b2c9c-729167170" //Todavía no vamos a poner el TOKET real: para hacer prueba vamos a usar USUARIOS DE PRUEBA.
 });
 
-        
-
 //Server
 
-app.get('/checkout', (req,res) => {
-    res.send(`<h1>Hola desde checkout  </h1>`)
+app.post('/checkout', (req,res) => {
+    // res.send(`<h1>Hola desde checkout  </h1>`)
+
+    // Crea un objeto de preferencia
+    let preference = {
+        items: [
+          {
+            title: req.body.title,
+            unit_price: parseInt(req.body.price),
+            quantity: 1,
+          }
+        ]
+      };
+      
+      mercadopago.preferences.create(preference)
+      .then(function(response){
+
+        // console.log(response.body);
+        res.redirect(response.body.init_point); 
+
+      }).catch(function(error){
+        console.log(error);
+
+      });
 })
 
 
