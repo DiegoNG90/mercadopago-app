a5c9fd10d70d05f26e2ad31c335704850df31024
Diego <diego.nicolas.gandara@gmail.com>
2021-03-15 23:07:34 -0300

    Como ahora trabajamos directamente con el front-end, apretamos el boton de pago y vamos a ver todo desde la interfaz de pago de mercado pago, dejo un comentario que explica el uso de ambos users: uno vendedor -el que pone la access_token- y el otro comprador, que inicia sesion en mercadopago y pone tarjeta, cuotas, etc. Vale la pena mencionar que mercadopago proporcion los datos de pago falsos/ de prueba: https://www.mercadopago.com.ar/developers/es/guides/online-payments/checkout-pro/test-integration

--- src/server.js
+++ src/server.js
@@ -16,6 +16,8 @@ app.use(express.urlencoded({extended: true}));
 // Agrega credenciales
 mercadopago.configure({
   access_token: "APP_USR-6308537224528879-031521-a5fcad24749177e4f6604d77f70b2c9c-729167170" //Todavía no vamos a poner el TOKET real: para hacer prueba vamos a usar USUARIOS DE PRUEBA.
+
+  //Hay que tener en cuenta que, dado que tenemos 2 fake_users, uno se va a usar como vendedor y otro como comprador. En el caso del access_token que se está usando acá, que corresponde al fake_user[0] (es decir, el primero de la array del archivo fake_users.json), lo lógico va a ser que, cuando vayamos al front y paguemos el producto, iniciemos sesion con el fake_user[1], es decir, el 2do, con lo cual éste va a cumplir el rol de comprador.
 });
 
 //Server
